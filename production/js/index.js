4d522fa03e4355cb7522b5dd5fdacd448a6af454
dinesh <dineshreddypaidi470@gmail.com>
2024-05-28 17:59:07 +0530

    prod

--- production/js/index.js
+++ production/js/index.js
@@ -1,21 +1,22 @@
 var header = document.getElementById('header');
 var img = document.getElementById("profilepic")
 var nav = document.getElementById('nav');
-nav.style.marginTop = '280px';
-img.style.height = '140px';
-img.style.width = '140px';
+nav.style.marginTop = '230px';
+img.style.height = '120px';
+img.style.width = '120px';
+header.style.height = '200px';
 
 function scrool() {
     if (window.scrollY > 1) {
-        header.style.height = '180px';
+        header.style.height = '170px';
         img.style.height = '100px';
         img.style.width = '100px';
         nav.style.marginTop = '250px';
     } else {
-        header.style.height = '240px';
-        img.style.height = '140px';
-        img.style.width = '140px';
-        nav.style.marginTop = '290px';
+        header.style.height = '200px';
+        img.style.height = '120px';
+        img.style.width = '120px';
+        nav.style.marginTop = '230px';
     }
 }
 
